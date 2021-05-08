82ba73228875b9da97828366bcdead28c8ee3b5c
Randy AP <rrandy.kun@gmail.com>
2021-05-09 04:53:10 +0700

    feat: menambahkan update dan delete catatan

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,10 @@
-const { addNoteHandler, getAllNotesHandler } = require('./handler');
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
 
 const routes = [
   {
@@ -10,6 +16,21 @@ const routes = [
     method: 'GET',
     path: '/notes',
     handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
   },
 ];
 
